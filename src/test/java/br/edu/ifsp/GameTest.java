aee8da0ced5e9983476fb044ff5202155a428268
Marcos Vinicius <marcos.carini22@gmail.com>
2023-09-26 20:36:13 -0300

    feat: Setup Inicial

---
+++ src/test/java/br/edu/ifsp/GameTest.java
@@ -0,0 +1,10 @@
+package br.edu.ifsp;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class GameTest {
+
+}
